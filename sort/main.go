69f796660a0f5d93ed871a8ce988aeda61273947
Lauro Santana <devlauross@gmail.com>
2025-02-16 19:46:32 -0300

    merge sort

--- sort/main.go
+++ sort/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	nums := []int{5, 4, 2, 1, 11, 99, 3, 50}
 	fmt.Println(quickSort(nums))
+	fmt.Println(mergeSort(nums))
 }
 
 func quickSort[E cmp.Ordered, S []E](list []E) []E {
@@ -46,4 +47,34 @@ func quickSort[E cmp.Ordered, S []E](list []E) []E {
 		right = append(right, v)
 	}
 	return append(append(quickSort(left), pivot), quickSort(right)...)
+}
+
+func mergeSort[S ~[]E, E cmp.Ordered](list S) S {
+	if len(list) <= 1 {
+		return list
+	}
+
+	middle := len(list) / 2
+
+	return merge(mergeSort(list[:middle]), mergeSort(list[middle:]))
+}
+
+func merge[S ~[]E, E cmp.Ordered](left, right S) S {
+	result := make(S, 0, len(left)+len(right))
+
+	l, r := 0, 0
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			result = append(result, left[l])
+			l++
+			continue
+		}
+		result = append(result, right[r])
+		r++
+	}
+
+	result = append(result, left[l:]...)
+	result = append(result, right[r:]...)
+
+	return result
 }
